0ecb0a0465daf8861a3939b1765952b2b9f1eb27
thefcraft <lakshsisodiya547@gmail.com>
2026-05-23 11:07:19 +0530

    minor changes for inference

--- web/app.py
+++ web/app.py
@@ -29,7 +29,10 @@ templates = Jinja2Templates(directory=BASE_DIR.joinpath("templates"))
 
 
 config = Config.from_dotenv(dotenv_path=BASE_DIR.joinpath("..", ".env"))
-
+SESSION_BASED: bool = True
+SORTED_SONG: bool = False
+SESSION_MOOD_CURR_WEIGHT: float = 0.75
+RECOMMEND_WATCHED: bool = False
 
 # =========================
 # LOAD MODEL
@@ -53,8 +56,15 @@ collection = client.get_or_create_collection(name=config.vector_db_collection_na
 
 processor = InferenceAudioProcessor(config=config)
 
+songs = os.listdir(config.music_dir)
+if SORTED_SONG:
+    songs.sort()
+else:
+    import random
 
-songs = sorted(os.listdir(config.music_dir))
+    random.shuffle(songs)
+session_mood_embedding: list[np.ndarray] | None = None
+history: set[str] = set()
 
 
 # =========================
@@ -79,10 +89,18 @@ def get_song_embeddings(song_path: Path):
 
 @torch.no_grad()
 def recommend(song_name: str, top_k: int = 10):
+    global session_mood_embedding
     song_path = config.music_dir.joinpath(song_name)
 
     embeddings = get_song_embeddings(song_path)
-
+    if SESSION_BASED:
+        if session_mood_embedding is None:
+            session_mood_embedding = embeddings
+        else:
+            session_mood_embedding = [
+                SESSION_MOOD_CURR_WEIGHT * i + (1 - SESSION_MOOD_CURR_WEIGHT) * j
+                for i, j in zip(embeddings, session_mood_embedding)
+            ]
     results = collection.query(
         query_embeddings=[
             *embeddings,
@@ -92,6 +110,10 @@ def recommend(song_name: str, top_k: int = 10):
         where={
             "filename": {
                 "$ne": song_name,
+            }
+            if RECOMMEND_WATCHED
+            else {
+                "$nin": list((*history, song_name)),
             }
         },
     )
@@ -162,7 +184,9 @@ async def get_recommendations(songid: str):  # pyright: ignore[reportUnknownPara
 
 @app.get("/music/{songid}")
 async def stream_music(songid: str):
+    global history
     song_name = urlsafe_b64decode(songid.encode()).decode()
+    history.add(song_name)
     file_path = config.music_dir.joinpath(song_name)
 
     return FileResponse(file_path)
